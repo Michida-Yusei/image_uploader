4c4ac08dc7603c8cc8db9c5234d7c851a64465b9
道田 <michida@mediaxis.jp>
2020-10-26 09:17:20 +0900

    画面実装

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,26 +1,51 @@
 <template>
   <div class="hello">
-    <vue-dropzone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions"></vue-dropzone>
+
+    <vue-dropzone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions"
+                  v-on:vdropzone-sending="sendingEvent"
+                  v-on:vdropzone-removed-file="removeEvent"
+    ></vue-dropzone>
+
   </div>
 </template>
 
 <script>
-  // vue2-dropzone と vue2-dropzone用のcssをimport
   import vue2Dropzone from 'vue2-dropzone'
   import 'vue2-dropzone/dist/vue2Dropzone.min.css'
+  import axios from 'axios'
 
   export default {
     name: 'HelloWorld',
     data: function () {
       return {
         dropzoneOptions: {
-          url: `http://localhost:8888/images`,
-          method: 'post'
+          url: `http://localhost/8888/images`,
+          method: 'post',
+          addRemoveLinks: 'true',
+          dictDefaultMessage: "<i class='fa fa-cloud-upload'></i>UPLOAD ME"
         }
       }
     },
     components: {
       vueDropzone: vue2Dropzone
+    },
+    methods: {
+      // formデータとして fileに付けられた任意のuuidを付加
+      sendingEvent: function (file, xhr, formData) {
+        formData.append('uuid', file.upload.uuid)
+      },
+      removeEvent: function (file) {
+        axios
+          .delete(`http://localhost:8888/images/${file.upload.uuid}`)
+          .then(res => {
+            console.log(res.data)
+          }).catch(err => {
+            console.error(err)
+          })
+      }
     }
   }
 </script>
+<style>
+  @import url("https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+</style>
