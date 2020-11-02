b6d00d5ae35c9877fdfba7d4e02e4c9f2807e47a
道田 <michida@michida.local>
2020-11-02 14:16:25 +0900

    パッケージを読み込もうとするとコンパイルエラーとなる為、一旦main.goに全て記載

--- server/main.go
+++ server/main.go
@@ -1,30 +1,71 @@
 package main // import "server"
 
 import (
-  "./handler"
+    "fmt"
+    "github.com/gin-gonic/gin" // フレームワーク
+    "net/http"
+    "os"
 
-  "github.com/gin-contrib/cors" // gin 用のCORS設定パッケージ
-  "github.com/gin-gonic/gin"    // フレームワーク
+    "github.com/gin-contrib/cors"
 )
 
 func main() {
     r := gin.Default()
 
-
-    r.GET("/", func(c *gin.Context) {
-      c.JSON(200, gin.H{
-        "message" : "ping",
-      })
-    })
     r.Use(cors.New(cors.Config{
-     AllowOrigins: []string{"http://localhost:8080"},
-     AllowMethods: []string{"GET", "POST", "DELETE", "OPTIONS"},
-     AllowHeaders: []string{"*"},
+        AllowOrigins: []string{"http://localhost:8080"},
+        AllowMethods: []string{"GET", "POST", "DELETE", "OPTIONS"},
+        AllowHeaders: []string{"*"},
     }))
 
-    r.POST("/images", handler.Upload)
+    r.GET("/", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "message" : "ping",
+        })
+    })
 
-    r.DELETE("/images/:uuid", handler.Delete)
+    r.POST("/images", Upload)
+
+    r.DELETE("/images/:uuid", Delete)
 
     r.Run(":8888")
+
+}
+
+
+/**
+ * Upload upload files.
+ * gin.Context にはアップロードされたfile達を処理するための、メソッドがいくつか生えている。
+ */
+func Upload(c *gin.Context) {
+    form, _ := c.MultipartForm()
+    files := form.File["file"]
+
+    // uuid を所得
+    uuid := c.PostForm("uuid")
+
+    for _, file := range files {
+        // ファイル名にuuidを仕込む
+        err := c.SaveUploadedFile(file, "images/"+uuid+".png")
+        if err != nil {
+            c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+        }
+    }
+
+    c.String(http.StatusOK, fmt.Sprintf("%d files uploaded!", len(files)))
+}
+
+/**
+ * Delete remove file.
+ */
+func Delete(c *gin.Context) {
+    uuid := c.Param("uuid")
+
+    err := os.Remove(fmt.Sprintf("images/%s.png", uuid))
+    if err != nil {
+        fmt.Println(err.Error())
+        c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+        return
+    }
+    c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("id: %s is deleted!", uuid)})
 }
