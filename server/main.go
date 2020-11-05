cc2d9184fafed0d63dccba42ee8fa14ee1f788cf
道田 <michida@mediaxis.jp>
2020-11-06 05:44:12 +0900

    独自パッケージに移す

--- server/main.go
+++ server/main.go
@@ -1,12 +1,10 @@
 package main // import "server"
 
 import (
-    "fmt"
-    "github.com/gin-gonic/gin" // フレームワーク
-    "net/http"
-    "os"
+  "server/handler"
 
-    "github.com/gin-contrib/cors"
+  "github.com/gin-gonic/gin" // フレームワーク
+  "github.com/gin-contrib/cors"
 )
 
 func main() {
@@ -20,52 +18,15 @@ func main() {
 
     r.GET("/", func(c *gin.Context) {
         c.JSON(200, gin.H{
-            "message" : "ping",
+            "message" : "hello",
         })
     })
 
-    r.POST("/images", Upload)
+    r.POST("/images", handler.Upload)
 
-    r.DELETE("/images/:uuid", Delete)
+    r.DELETE("/images/:uuid", handler.Delete)
 
     r.Run(":8888")
 
 }
 
-
-/**
- * Upload upload files.
- * gin.Context にはアップロードされたfile達を処理するための、メソッドがいくつか生えている。
- */
-func Upload(c *gin.Context) {
-    form, _ := c.MultipartForm()
-    files := form.File["file"]
-
-    // uuid を所得
-    uuid := c.PostForm("uuid")
-
-    for _, file := range files {
-        // ファイル名にuuidを仕込む
-        err := c.SaveUploadedFile(file, "images/"+uuid+".png")
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
-        }
-    }
-
-    c.String(http.StatusOK, fmt.Sprintf("%d files uploaded!", len(files)))
-}
-
-/**
- * Delete remove file.
- */
-func Delete(c *gin.Context) {
-    uuid := c.Param("uuid")
-
-    err := os.Remove(fmt.Sprintf("images/%s.png", uuid))
-    if err != nil {
-        fmt.Println(err.Error())
-        c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
-        return
-    }
-    c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("id: %s is deleted!", uuid)})
-}
